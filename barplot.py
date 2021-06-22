6ffc3f5e4903525286f076f8e519ad93945f6f6b
Gops-8 <gopalpadhi8@hotmail.com>
2021-06-22 13:08:32 +0530

    added skill page

---
+++ barplot.py
@@ -0,0 +1,27 @@
+import numpy as np 
+import pandas as pd 
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+dict ={'skill':['Python','Pandas','Numpy','Machine Learning','Deep Learning','NLP','Hadoop','Pyspark','Flask','AWS'],
+       'expertise':[7,9,8,8,7,4,5,6,4,4]}
+
+df=pd.DataFrame(dict)
+
+sns.set_theme(style="dark")
+
+# Initialize the matplotlib figure
+f, ax = plt.subplots(figsize=(12, 5))
+
+
+# Plot the total crashes
+sns.set_color_codes("pastel")
+sns.barplot(y="skill", x="expertise", data=df,
+            label="Technical skill", color="b")
+
+ax.legend(ncol=2, loc="lower right", frameon=True)
+ax.set(xlim=(0, 10), ylabel="",
+       xlabel="Technical Skill")
+
+sns.despine(left=True, bottom=True)
+plt.savefig(r'images\barplot.png')
