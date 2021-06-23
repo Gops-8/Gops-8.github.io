78c2cd9668306dad69aa2eabdd018f50a19b9421
Gops-8 <gopalpadhi8@hotmail.com>
2021-06-24 00:39:43 +0530

    using csv inplace of dict

--- barplot.py
+++ barplot.py
@@ -3,12 +3,12 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-dict ={'skill':['Python','Pandas','Numpy','Machine Learning','Deep Learning','NLP','Hadoop','Pyspark','Flask','AWS'],
-       'expertise':[7,9,8,8,7,4,5,6,4,4]}
+# dict ={'skill':['Python','Pandas','Numpy','Machine Learning','Deep Learning','NLP','Hadoop','Pyspark','Flask','AWS'],
+       # 'expertise':[7,9,8,8,7,4,5,6,4,4]}
 
-df=pd.DataFrame(dict)
+df=pd.read_csv("skill.csv")
 
-sns.set_theme(style="dark")
+sns.set_theme(style="darkgrid")
 
 # Initialize the matplotlib figure
 f, ax = plt.subplots(figsize=(12, 5))
@@ -16,7 +16,7 @@ f, ax = plt.subplots(figsize=(12, 5))
 
 # Plot the total crashes
 sns.set_color_codes("pastel")
-sns.barplot(y="skill", x="expertise", data=df,
+sns.barplot(y="Skill", x="Score", data=df,
             label="Technical skill", color="b")
 
 ax.legend(ncol=2, loc="lower right", frameon=True)
